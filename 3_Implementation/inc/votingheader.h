1a3feb6c28472838c5eccbc418a41a53f9db4843
NagaVarmaUppalapati <52658653+NagaVarmaUppalapati@users.noreply.github.com>
2021-07-06 18:13:27 +0530

    Add files via upload

---
+++ 3_Implementation/inc/votingheader.h
@@ -0,0 +1,17 @@
+/**
+ * @brief 
+ * 
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char heading()
+	{
+		system("cls");
+
+	printf("\n\t\t\t\t\t\t     ANDHRA PRADESH\n");
+	printf("------------------------------------------------------------------------------------------------------------------------\n\n");
+	printf("\t\t\t\t\t  WELCOME TO THE ELECTION MANAGEMENT APP\n\n");
+	printf("\t\t\t\t\t\t  ************************\n");
+	printf("\n\n\n");
+	}
